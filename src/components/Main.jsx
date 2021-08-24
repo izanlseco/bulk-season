230d86a96442393c9f993bdf8c5b274cd9508b45
Izan Lopez Seco <IzanLS09@gmail.com>
2021-08-24 20:48:58 +0200

    initial configuration

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+
+const Main = () => {
+  return (
+    <>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </>
+  );
+};
+
+export default Main;
